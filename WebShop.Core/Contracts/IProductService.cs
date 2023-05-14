9c7fdee88091d585d6f42793fb3c4b0e3762baaa
maya-ivanova <65265183+maya-ivanova@users.noreply.github.com>
2023-05-14 23:00:45 +0300

    Added WebShop.Core project

---
+++ WebShop.Core/Contracts/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Core.Models;
+
+namespace WebShop.Core.Contracts
+{
+    /// <summary>
+    /// Manipulates product data
+    /// </summary>
+    public interface IProductService
+    {
+        /// <summary>
+        /// Gets all the products
+        /// </summary>
+        /// <returns>List of products</returns>
+        Task<IEnumerable<ProductDto>> GetAllAsync();
+    }
+}
---
+++ WebShop.Core/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Core.Contracts;
+using WebShop.Core.Models;
+
+namespace WebShop.Core.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IConfiguration config;
+        public ProductService(IConfiguration _config)
+        {
+            config = _config;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAllAsync()
+        {
+            string dataPath = config.GetSection("DataFiles:Products").Value;
+            string data = await File.ReadAllTextAsync(dataPath);
+
+            return JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(data);
+        }
+    }
+}
